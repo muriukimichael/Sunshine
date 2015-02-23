362b4fe7a7660adfb153af1854e5c9b7fddb1180
muriukimichael <muriukimichael@gmail.com>
2015-02-23 11:52:04 +0300

    Create and define a service

---
+++ app/src/main/java/com/pixelimpressions/www/sunshine/service/SunshineService.java
@@ -0,0 +1,19 @@
+package com.pixelimpressions.www.sunshine.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Created by mikie on 2/23/15.
+ */
+public class SunshineService extends IntentService {
+
+    public SunshineService() {
+        super("SunshineService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
